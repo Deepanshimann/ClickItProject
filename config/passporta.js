26d85615df93b8d4ff102c0728ea8d7414959cae
Deepanshimann <deepanshimann96@gmail.com>
2024-10-09 19:52:31 +0100

    changed environment

--- config/passporta.js
+++ config/passporta.js
@@ -8,7 +8,10 @@ const passport=require("passport")
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET ,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL:
+    process.env.NODE_ENV === "production"
+    ? "https://click-it-and-buy.onrender.com/auth/google/callback" 
+    : "http://localhost:3000/auth/google/callback", 
   },
   async function(accessToken, refreshToken, profile, cb) {
   try{
